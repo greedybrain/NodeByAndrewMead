e77a12d508c38f80aae7f3e1f8cac72174f69ea7
greedybrain <greedybrain18@gmail.com>
2021-01-03 17:51:58 -0500

    ...

--- task-app/mongodb.js
+++ task-app/mongodb.js
@@ -1,21 +1,21 @@
 // CRUD
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require("mongodb");
+// const MongoClient = mongodb.MongoClient;
 
-const connectionURL = "mongodb://127.0.0.1:27017";
-const databaseName = "task-app";
+// const connectionURL = "mongodb://127.0.0.1:27017";
+// const databaseName = "task-app";
 
-MongoClient.connect(
-	connectionURL,
-	{ useNewUrlParser: true, useUnifiedTopology: true },
-	(error, client) => {
-		if (error) return console.log("Unable to connect to database");
+// MongoClient.connect(
+// 	connectionURL,
+// 	{ useNewUrlParser: true, useUnifiedTopology: true },
+// 	(error, client) => {
+// 		if (error) return console.log("Unable to connect to database");
 
-		const db = client.db(databaseName);
+// 		const db = client.db(databaseName);
 
-		db.collection("users").insertOne({
-			name: "Naya Willis",
-			age: 29,
-		});
-	}
-);
+// 		db.collection("users").insertOne({
+// 			name: "Naya Willis",
+// 			age: 29,
+// 		});
+// 	}
+// );
--- task-app/src/db/mongoose.js
+++ task-app/src/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const connectionURL = "mongodb://127.0.0.1:27017/task-app-api";
+const connectionURL =
+	"mongodb+srv://wycbw91:360ec06a3f644c8de5c83e7ac0ccdd63@node-go.skajs.mongodb.net/task-app-api?retryWrites=true&w=majority";
 mongoose
 	.connect(connectionURL, {
 		useNewUrlParser: true,
@@ -10,53 +12,81 @@ mongoose
 	.then(() => console.log("Connected to database"))
 	.catch((err) => console.log(error));
 
-// const Task = mongoose.model("Task", {
-// 	description: {
-// 		type: String,
-// 		required: true,
-// 	},
-// 	completed: {
-// 		type: Boolean,
-// 		required: true,
-// 	},
-// });
-
-// const createTask = async () => {
-// 	const task = new Task({
-// 		description: "Take out the garbage",
-// 		completed: false,
-// 	});
-// 	try {
-// 		console.log(task);
-// 		await task.save();
-// 	} catch (error) {
-// 		console.log("Error is > ", error);
-// 	}
-// };
-
-const User = mongoose.model("User", {
-	name: {
+const Task = mongoose.model("Task", {
+	description: {
 		type: String,
+		required: true,
+		trim: true,
 	},
-	age: {
-		type: Number,
-		validate(value) {
-			if (value < 0) throw Error("Please enter a valid age");
-		},
+	completed: {
+		type: Boolean,
+		default: false,
 	},
 });
 
-const createUser = async () => {
-	const user = new User({
-		name: "Willis",
-		age: "Mike",
+const createTask = async () => {
+	const task = new Task({
+		description: "      Take out the garbage     ",
+		completed: false,
 	});
 	try {
-		await user.save();
-		console.log(user);
+		await task.save();
 	} catch (error) {
-		console.log("Error", error);
+		console.log("Error is > ", error);
 	}
 };
+
+createTask();
+
+// const User = mongoose.model("User", {
+// 	name: {
+// 		type: String,
+// 		required: true,
+// 		trim: true,
+// 	},
+// 	email: {
+// 		type: String,
+// 		required: true,
+// 		trim: true,
+// 		lowercase: true,
+// 		validate(value) {
+// 			if (!validator.isEmail(value)) throw Error("Invalid email");
+// 		},
+// 	},
+// 	password: {
+// 		type: String,
+// 		required: true,
+// 		trim: true,
+// 		minlength: 6,
+// 		validate(value) {
+// 			if (value.toLowerCase().includes("password"))
+// 				throw Error(
+// 					"Password is invalid, password cannot contain the word 'password'"
+// 				);
+// 		},
+// 	},
+// 	age: {
+// 		type: Number,
+// 		default: 0,
+// 		validate(value) {
+// 			if (value < 0) throw Error("Please enter a valid age greater than 0");
+// 		},
+// 	},
+// });
+
+// const createUser = async () => {
+// 	const user = new User({
+// 		name: "Naya Willis",
+// 		email: "WILLISNAYA@gmail.com",
+// 		password: "newpassword",
+// 		age: 29,
+// 	});
+// 	try {
+// 		await user.save();
+// 		console.log(user);
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// };
 
 // createUser();
